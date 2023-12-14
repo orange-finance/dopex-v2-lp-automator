eedadfc957f90f1f2adf3b96464fa622f484e7eb
maguro <maguro.abs@gmail.com>
2023-12-14 11:23:25 +0900

    test: redeem dopex shares fully locked

--- test/Automator/Redeem.t.sol
+++ test/Automator/Redeem.t.sol
@@ -57,6 +57,35 @@ contract TestAutomatorRedeem is Fixture {
         assertApproxEqRel(WETH.balanceOf(alice), 1.3 ether, 0.0005e18); // max 0.05% diff (swap fee)
     }
 
+    function test_redeem_dopexPositionFullyLocked() public {
+        _depositFrom(bob, 1 ether);
+        _depositFrom(alice, 1.3 ether);
+
+        // mint dopex position using 1.3 WETH
+        // liquidity(-199330, -199320, 1.3 WETH, 0 USDC.e) = 122158608466771138 (current tick: -199349)
+        _rebalanceMintSingle(-199330, 122158608466771138);
+
+        // use all liquidity in dopex (can be calculated from TokenIdInfo.totalLiquidity - TokenIdInfo.liquidityUsed)
+        _useDopexPosition(-199330, -199320, 122933243781049490);
+
+        // now the balance of WETH in automator ≈ 1 ether
+        vm.prank(alice);
+        (uint256 _assets, IAutomator.LockedDopexShares[] memory _locked) = automator.redeem(1.3 ether, 0);
+
+        /**
+         * 1.3 ether * 1 ether (redeemable WETH in automator) / 2.3 ether(automator total supply) = 565217391304347826
+         * NOTE: some error in calculation because WETH is not actually used to mint dopex position as expected (less amount is used)
+         */
+        assertApproxEqRel(_assets, 565217391304347826, 0.0001e18); // allow 0.01% diff
+        assertEq(_locked.length, 1);
+
+        // keccak256(abi.encode(uniV3Handler, pool, -199330, -199320))
+        assertEq(_locked[0].tokenId, 99415793022624657546894716849135746671790202110748929622730375369874075708201);
+
+        // alice locked shares = 121557834267548568 (automator's locked shares) * 1.3 ether (redeemable WETH in automator) / 2.3 ether (automator total supply)
+        assertEq(_locked[0].shares, 68706601977310060);
+    }
+
     function test_redeem_dopexPositionPartiallyLocked() public {
         _depositFrom(bob, 1 ether);
         _depositFrom(alice, 1.3 ether);
