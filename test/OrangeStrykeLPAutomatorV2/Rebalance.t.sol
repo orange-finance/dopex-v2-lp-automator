9ee6479e9f9deb41fc7bb79c62aec8dda2e3e64f
maguro <maguro.abs@gmail.com>
2024-02-29 21:25:08 +0900

    test: pass automator address as the argument

--- test/OrangeStrykeLPAutomatorV2/Rebalance.t.sol
+++ test/OrangeStrykeLPAutomatorV2/Rebalance.t.sol
@@ -9,6 +9,7 @@ import {Test, stdJson} from "forge-std/Test.sol";
 import {WETH_USDC_OrangeStrykeLPAutomatorV2Harness} from "./OrangeStrykeLPAutomatorV2Harness.sol";
 import {IOrangeStrykeLPAutomatorV2} from "./../../contracts/interfaces/IOrangeStrykeLPAutomatorV2.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Strings} from "@openzeppelin//contracts/utils/Strings.sol";
 
 contract Fixture is Test {
     using stdJson for string;
@@ -32,41 +33,10 @@ contract TestOrangeStrykeLPAutomatorV2Rebalance is Fixture {
         super.setUp();
     }
 
-    function test_automator() public {
-        automator.deposit(100 ether, alice);
-
-        assertEq(automator.automator().balanceOf(alice), 99999000000000000000);
-    }
-
     function test_rebalance_flashLoanAndSwap_Skip() public {
         automator.deposit(100 ether, alice);
 
-        // get swap data
-        // string[] memory buildSwapData = stdJson.readStringArray(
-        //     // solhint-disable-next-line quotes
-        //     '["node", "test/OrangeStrykeLPAutomatorV2/kyberswap.mjs", "-i", "weth", "-o", "usdc", "-a", 50, "-s", "0xb0c757bC94704246Ce0552b5Ccc1A547c0633914"]',
-        //     "."
-        // );
-        string[] memory buildSwapData = stdJson.readStringArray(
-            string.concat(
-                "node",
-                "test/OrangeStrykeLPAutomatorV2/kyberswap.mjs",
-                "-i",
-                "weth",
-                "-o",
-                "usdc",
-                "-a",
-                "50",
-                "-s",
-                // TODO: replace with actual automator address
-                // ? how to convert address to string
-                "<automator-address>"
-            ),
-            "."
-        );
-        bytes memory swapData = vm.ffi(buildSwapData);
-
-        (address router, bytes memory swapCallData) = abi.decode(swapData, (address, bytes));
+        (address router, bytes memory swapCallData) = _buildSwapData(address(automator.automator()), 50);
 
         emit log_named_uint("vault weth balance before: ", IERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1).balanceOf(address(automator.automator()))); // prettier-ignore
         emit log_named_uint("vault usdc balance before: ", IERC20(0xaf88d065e77c8cC2239327C5EDb3A432268e5831).balanceOf(address(automator.automator()))); // prettier-ignore
@@ -87,5 +57,25 @@ contract TestOrangeStrykeLPAutomatorV2Rebalance is Fixture {
 
         emit log_named_uint("vault weth balance after: ", IERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1).balanceOf(address(automator.automator()))); // prettier-ignore
         emit log_named_uint("vault usdc balance after: ", IERC20(0xaf88d065e77c8cC2239327C5EDb3A432268e5831).balanceOf(address(automator.automator()))); // prettier-ignore
+    }
+
+    function _buildSwapData(
+        address sender,
+        uint256 amount
+    ) internal returns (address router, bytes memory swapCalldata) {
+        string[] memory buildSwapData = new string[](10);
+        buildSwapData[0] = "node";
+        buildSwapData[1] = "test/OrangeStrykeLPAutomatorV2/kyberswap.mjs";
+        buildSwapData[2] = "-i";
+        buildSwapData[3] = "weth";
+        buildSwapData[4] = "-o";
+        buildSwapData[5] = "usdc";
+        buildSwapData[6] = "-a";
+        buildSwapData[7] = Strings.toString(amount);
+        buildSwapData[8] = "-s";
+        buildSwapData[9] = Strings.toHexString(uint256(uint160(sender)));
+
+        bytes memory swapData = vm.ffi(buildSwapData);
+        (router, swapCalldata) = abi.decode(swapData, (address, bytes));
     }
 }
