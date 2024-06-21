fad07c1ee98469970ff49dbe5e78057cc0c51add
maguro <maguro.abs@gmail.com>
2024-06-21 15:20:17 +0900

    build: pancake deploy

--- deploy/automator/v2/params/arbitrum/pancake-usdc-arb.ts
+++ deploy/automator/v2/params/arbitrum/pancake-usdc-arb.ts
@@ -1,7 +1,7 @@
 import { V2Parameters } from '../schema'
 
 const usdc_arb: V2Parameters = {
-  id: 'USDC-ARB',
+  id: 'Pancake-USDC-ARB',
   balancer: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
   admin: '0x38E4157345Bd2c8Cf7Dbe4B0C75302c2038AB7Ec',
 }
--- deploy/automator/v2/params/arbitrum/pancake-weth-usdc.ts
+++ deploy/automator/v2/params/arbitrum/pancake-weth-usdc.ts
@@ -1,7 +1,7 @@
 import { V2Parameters } from '../schema'
 
 const weth_usdc: V2Parameters = {
-  id: 'WETH-USDC',
+  id: 'Pancake-WETH-USDC',
   balancer: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
   admin: '0x38E4157345Bd2c8Cf7Dbe4B0C75302c2038AB7Ec',
 }
