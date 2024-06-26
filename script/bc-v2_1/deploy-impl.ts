571296371065e7285836a27610b044ed5597d76b
maguro <maguro.abs@gmail.com>
2024-06-26 23:12:15 +0900

    build: v2_1 upgrade script

---
+++ script/bc-v2_1/deploy-impl.ts
@@ -0,0 +1,15 @@
+import hre from 'hardhat'
+
+// to use in fork test, deploy implementation separately
+async function main() {
+  const impl = await hre.ethers.deployContract(
+    'BackwardCompatibleOrangeStrykeLPAutomatorV2_1',
+  )
+
+  console.log(`implementation deployed to: ${impl.target}`)
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
